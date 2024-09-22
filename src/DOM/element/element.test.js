b30c4b39238cbb42eba39504b826f9b2a75e6e60
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2024-09-22 16:40:06 -0600

    test(dom): add unit tests of getting the HTML of an element

--- src/DOM/element/element.test.js
+++ src/DOM/element/element.test.js
@@ -138,4 +138,28 @@ describe('Element', () => {
             );
         });
     });
+
+    describe('getHTML', () => {
+        it('returns the safe  HTML content of an element', () => {
+            Element.setHTML(
+                document.body,
+                `<div data-test="get-html">
+<label id="testLabel">Element.getHTML</label>
+</div>
+<iframe src="https://google.com" frameborder="5"></iframe>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>`,
+                true,
+            );
+
+            const htmlContent = Element.getHTML(document.body);
+
+            expect(document.querySelector('iframe,link,script')).to.exist;
+            expect(htmlContent).not.to.match(/<iframe|<link|<script/);
+            expect(htmlContent.trimEnd()).to.be
+                .equal(`<div data-test="get-html">
+<label id="testLabel">Element.getHTML</label>
+</div>`);
+        });
+    });
 });
