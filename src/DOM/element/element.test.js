946a196f672c48e12f1dec653f54eead2602a280
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2024-09-22 16:37:08 -0600

    test(dom): add unit tests of creating an element

---
+++ src/DOM/element/element.test.js
@@ -0,0 +1,111 @@
+import { expect } from '@open-wc/testing';
+
+import { Element } from './element';
+
+describe('Element', () => {
+    describe('create', () => {
+        it('throws an error if no element name is provided', () => {
+            const message = "Element creation requires a 'name' property.";
+
+            expect(() => Element.create()).to.throw(message);
+            expect(() => Element.create(null)).to.throw(message);
+        });
+
+        it('creates an element with the provided name', () => {
+            const name = 'input';
+            const element = Element.create({
+                name,
+            });
+
+            expect(element).to.be.an.instanceOf(HTMLElement);
+            expect(element).to.have.tagName(name);
+        });
+
+        it('creates an element with the provided CSS classes', () => {
+            const classes = ['class1', 'class2'];
+            const element = Element.create({
+                name: 'input',
+                class: classes,
+            });
+
+            expect(element).to.have.class(classes[0]);
+            expect(element).to.have.class(classes[1]);
+            expect(element.classList).to.have.lengthOf(classes.length);
+        });
+
+        it('creates an element with the provided attributes', () => {
+            const attributes = {
+                type: 'text',
+                placeholder: 'Enter text here',
+            };
+            const element = Element.create({
+                name: 'input',
+                attr: attributes,
+            });
+
+            expect(element).to.have.attribute('type', attributes.type);
+            expect(element).to.have.attribute(
+                'placeholder',
+                attributes.placeholder,
+            );
+        });
+
+        it('creates an element with the provided text content', () => {
+            const text = 'Hello, World!';
+            const element = Element.create({
+                name: 'label',
+                innerText: text,
+            });
+
+            expect(element).to.have.text(text);
+        });
+
+        it('creates an element with the provided HTML content', () => {
+            const html = '<strong>Hello, World!</strong>';
+            const element = Element.create({
+                name: 'div',
+                innerHTML: html,
+            });
+
+            expect(element).to.have.html(html);
+        });
+
+        it('creates an element with children elements', () => {
+            const children = [
+                Element.create({ name: 'span', innerText: 'Child 1' }),
+                Element.create({ name: 'span', innerText: 'Child 2' }),
+            ];
+            const element = Element.create({
+                name: 'div',
+                children,
+            });
+
+            expect(element.children).to.have.lengthOf(children.length);
+            expect(element.children[0]).to.have.text('Child 1');
+            expect(element.children[1]).to.have.text('Child 2');
+        });
+
+        it('creates a draggable element', () => {
+            const element = Element.create({
+                name: 'div',
+                draggable: true,
+            });
+
+            expect(element).to.have.attribute('draggable');
+        });
+
+        it('creates an element with CSS style', async () => {
+            const element = Element.create(
+                {
+                    name: 'button',
+                    innerText: 'Hello',
+                    style: 'color: white; display: block',
+                },
+                (element) => document.body.appendChild(element),
+            );
+
+            expect(element).to.have.style('color', 'rgb(255, 255, 255)');
+            expect(element).to.have.style('display', 'block');
+        });
+    });
+});
